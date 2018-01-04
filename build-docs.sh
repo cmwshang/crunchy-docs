90902c50dcd1e9965078349c526d992f6b431d37
xenophenes <sarah.conway@crunchydata.com>
2018-01-03 21:59:48 -0800

    switch build-docs to generate pages into matching filenames

--- build-docs.sh
+++ build-docs.sh
@@ -19,29 +19,29 @@ OPERATION=$2
 if [[ "$#" -le 1 ]]; then
   echo "Usage:"
   echo "   $0 <name> <operation>"
-  echo "   - name: install | examples | metrics | pitr | smoketest | dedicated | containers | backrest"
+  echo "   - name: readme | install | examples | metrics | pitr | smoketest | dedicated | containers | backrest"
   echo "   - operation: create | delete"
   exit
 fi
 
 function create {
-  python asciidoc.py --no-header-footer -o ./templates/pages/$NAME.html ./$NAME.adoc
+  python asciidoc.py --no-header-footer -o ./templates/pages/temp.html ./$NAME.adoc
 
   # This won't be necessary to include after the modified converter
 
   echo "{%extends \"base.html\" %}
   {%block title%}Installation | Crunchy Container Suite{%endblock%}
   {%block pagetitle%}Crunchy Container Suite{%endblock%}
-  {%block content%}" >> ./templates/pages/index.html
+  {%block content%}" >> ./templates/pages/$NAME.html
 
-  cat ./templates/pages/$NAME.html >> ./templates/pages/index.html
-  echo "{% endblock %}" >> ./templates/pages/index.html
+  cat ./templates/pages/temp.html >> ./templates/pages/$NAME.html
+  echo "{% endblock %}" >> ./templates/pages/$NAME.html
 
-  rm ./templates/pages/$NAME.html
+  rm ./templates/pages/temp.html
 }
 
 function delete {
-  rm ./templates/pages/index.html
+  rm ./templates/pages/$NAME.html
 }
 
 case $OPERATION in
