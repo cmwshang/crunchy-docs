e0e583a48c71acffafc3adee7e081ea5d351948d
xenophenes <sarah.conway@crunchydata.com>
2018-01-05 11:48:44 -0800

    leave out h3 for now

--- static/js/main.js
+++ static/js/main.js
@@ -30,14 +30,3 @@ $(document).ready(function() {
   })
 
 })
-
-$(document).ready(function() {
-
-  $("h3").each(function() {
-    var text = $(this).text()
-    var id = $(this).attr("id")
-    var navLink = "<li><a href='#" + id + "'>" + text + "</a></li>"
-    $("#nav").append(navLink)
-  })
-
-})
